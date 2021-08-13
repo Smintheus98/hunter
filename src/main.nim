06aa2783f06833d11a4fd1dec64591cbeb421a51
Yannic Kitten <yannic.kitten@outlook.de>
2021-08-13 16:46:16 +0200

    Added debug-vectors and simple but working flee-mode

--- src/main.nim
+++ src/main.nim
@@ -1,6 +1,6 @@
 import nico, nico/backends/common
 import utils
-import std/[fenv, os, strformat]
+import std/[fenv, os, strformat, random]
 
 #[
 colors: 
@@ -36,7 +36,7 @@ const
   #win_r = win_w / win_h   # ==1: square, >1: rectangle (lying), <1: rectangle (standing)
 
 discard fenv.fesetround(FE_TONEAREST)
-
+randomize()
 
 
 proc createPlayer(pos = (x: win_hw, y: win_hh)): Figure =
@@ -48,13 +48,13 @@ proc createHunter(pos: Position): Figure =
 proc createHunters(n: int): seq[Figure] =
   result = @[]
   if n >= 1:
-    result.add createHunter((win_w, win_hh))
+    result.add createHunter((win_w-hunter_radius, win_hh))
   if n >= 2:
-    result.add createHunter((0, win_hh))
+    result.add createHunter((0+hunter_radius, win_hh))
   if n >= 3:
-    result.add createHunter((win_hw, 0))
+    result.add createHunter((win_hw, 0+hunter_radius))
   if n >= 4:
-    result.add createHunter((win_hw, win_h))
+    result.add createHunter((win_hw, win_h-hunter_radius))
 
 proc move(f: var Figure, d: Direction) =
   var 
@@ -77,17 +77,64 @@ proc hunt(h: var Figure, p: Figure) =
 
 proc flee(h: var Figure, p: Figure) =
   let
-    d_away: Direction = toDirection(h.pos - p.pos)
-    d_away_norm = d_away.normalize
-    d_middle = toDirection((win_hw, win_hh) - h.pos)
-    d_middle_norm = d_middle.normalize
-    d_sum = d_away + d_middle
-    d_sum_norm = d_away_norm + d_middle_norm
-  # TODO: do smart magic to flee from the player
+    d_awy = toDirection(h.pos - p.pos)
+    d_awy_nrml = d_awy.normalize
+    d_mdl = toDirection((win_hw, win_hh) - h.pos)
+    d_mdl_nrml = d_mdl.normalize
+    d_sum = d_awy + d_mdl
+    d_sum_nrml = d_awy_nrml + d_mdl_nrml
+    dd_dotprod = dotp(d_awy_nrml, d_mdl_nrml) 
+  # TODO: do some smart magic to flee from the player
+  var d_flee: Direction
+  
+  if d_mdl.norm() < 150:
+    d_flee = d_awy
+  elif d_awy.norm() < 150:
+    d_flee = d_awy
+  elif dd_dotprod.abs > 0.99:
+    d_flee.x = d_awy.y
+    d_flee.y = d_awy.x
+    if dd_dotprod.abs == 1:
+      if rand(1.0) < 0.5:
+        d_flee.x *= -1
+      else:
+        d_flee.y *= -1
+    elif dd_dotprod > 0:
+      d_flee.x *= -1
+    elif dd_dotprod < 0:
+      d_flee.y *= -1
+  else:
+    d_flee = d_sum
+  h.move(d_flee)
 
 proc draw(f: Figure) =
   setColor(f.color)
   circfill(f.pos.x, f.pos.y, f.radius)
+
+proc draw_debug(f1, f2: Figure) =
+  var 
+    endpos: Position
+  let
+    d_awy = toDirection(f1.pos - f2.pos)
+    d_awy_nrml = d_awy.normalize(30)
+    d_mdl = toDirection((win_hw, win_hh) - f1.pos)
+    d_mdl_nrml = d_mdl.normalize(30)
+    d_sum = d_awy + d_mdl
+    d_sum_nrml = d_awy_nrml + d_mdl_nrml
+
+  setColor(MyColors("yellow"))
+  endpos = f1.pos + d_awy
+  nico.line(f1.pos.x, f1.pos.y, endpos.x, endpos.y)
+  endpos = f1.pos + d_mdl
+  nico.line(f1.pos.x, f1.pos.y, endpos.x, endpos.y)
+  setColor(MyColors("green"))
+  endpos = f1.pos + d_awy_nrml
+  nico.line(f1.pos.x, f1.pos.y, endpos.x, endpos.y)
+  endpos = f1.pos + d_mdl_nrml
+  nico.line(f1.pos.x, f1.pos.y, endpos.x, endpos.y)
+  setColor(MyColors("cyan"))
+  endpos = f1.pos + d_sum
+  nico.line(f1.pos.x, f1.pos.y, endpos.x, endpos.y)
 
 proc keysToDirection(): Direction =
   result = (0.0, 0.0)
@@ -104,6 +151,7 @@ proc keysToDirection(): Direction =
 var
   caught = false
   startscreen = true
+  hunter_mode = flee
   player: Figure
   hunters: seq[Figure] = @[]
   hunter_num: int = 1
@@ -133,11 +181,15 @@ proc gameUpdate(dt: float32) =
   if not caught and not startscreen:
     for hunter in hunters:
       if distance(player, hunter) < player.radius + hunter.radius:
+        # TODO: different behaviour when touched in hunting/fleeing
         caught = true
         return
     player.move(keysToDirection())
     for hunter in hunters.mitems:
-      hunter.hunt(player)
+      if hunter_mode == hunt:
+        hunter.hunt(player)
+      else:
+        hunter.flee(player)
 
 proc gameDraw() =
   cls()
@@ -150,6 +202,9 @@ proc gameDraw() =
   player.draw()
   for hunter in hunters:
     hunter.draw
+    hunter.draw_debug(player)
+
+
   setColor(MyColors("darkest-gray"))
   nico.boxfill(0, win_h, gui_w, gui_h-win_h)
   setColor(MyColors("darker-gray"))
